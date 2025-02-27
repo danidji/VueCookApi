aa38d54467f1a7329f606de8742a20082a054c30
danGbe <73373221+danidji@users.noreply.github.com>
2025-02-27 17:53:05 +0100

    Feat: add keyvault

--- Program.cs
+++ Program.cs
@@ -1,8 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using VueCookApi.Data;
 using VueCookApi.Services;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultUrl = builder.Configuration["keyVaultUrl"];
+
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+     Console.WriteLine($"client {client}"); 
+    KeyVaultSecret secret = client.GetSecret("DbConnectionString").Value;    
+
+     Console.WriteLine($"Secret récupéré : {secret.Value}"); 
+    builder.Configuration["ConnectionStrings:DefaultConnection"] = secret.Value;
+}
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
